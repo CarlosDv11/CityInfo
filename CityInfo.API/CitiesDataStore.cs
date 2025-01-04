89dbd9a474477aff72a9228d2dec8e8119005e82
Carlos <carloselima2001@gmail.com>
2025-01-03 23:49:00 -0300

    add: PointsOfInterest with dummy data initialization

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -5,18 +5,33 @@ namespace CityInfo.API
     public class CitiesDataStore
     {
         public List<CityDto> Cities { get; set; }
-        public static CitiesDataStore Current { get;} = new CitiesDataStore();
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
 
         public CitiesDataStore()
         {
             Cities = new List<CityDto>
             {
-                // init dummy data
+                // Initialize dummy data
                 new CityDto
                 {
                     Id = 1,
                     Name = "New York City",
-                    Description = "The one with that big park."
+                    Description = "The one with that big park.",
+                    PointOfInterests = new List<PointOfInterestDto>
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the United States."
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan."
+                        }
+                    }
                 },
                 new CityDto
                 {
--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -6,6 +6,14 @@
         public string? Name { get; set; } 
         public string? Description { get; set; }
 
+        public int NumberOfPointsOfInterest 
+        { 
+            get
+            {
+                return PointOfInterests.Count;
+            }
+        }
+
         public ICollection<PointOfInterestDto> PointOfInterests { get; set; }
            =new List<PointOfInterestDto>();
     }

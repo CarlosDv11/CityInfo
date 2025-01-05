c80b887b2466916c69b515fcef7498bfd01a3839
Carlos <carloselima2001@gmail.com>
2025-01-04 23:39:15 -0300

    Add: PointsOfInterestController with endpoints for fetching points of interest

---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : Controller
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointOfInterests);
+        }
+
+        [HttpGet("{pointsofinterestid}")]
+
+        public ActionResult<PointOfInterestDto> GetPointOfinterest(int cityId, int pointsofinterestid) 
+        { 
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null) 
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointOfInterests
+                .FirstOrDefault(c => c.Id == pointsofinterestid);
+            if (pointOfInterest == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterest);
+        }
+    }
+}
